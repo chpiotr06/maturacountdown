109dcf0c551599bf45b39c70b975746e2c1aae8b
chpiotr06 <chpiotr06@gmail.com>
2021-10-01 09:12:44 +0200

    Working countdown

---
+++ src/index.js
@@ -0,0 +1,26 @@
+setInterval(timerFunction, 1000);
+
+function displayTimer(days, hours, minutes, seconds) {
+  const daysHtml = document.getElementById('days-container')
+  const hoursHtml = document.getElementById('hours-container')
+  const minutesHtml = document.getElementById('minutes-container')
+  const secondsHtml = document.getElementById('seconds-container')
+  daysHtml.innerHTML = days
+  hoursHtml.innerHTML = hours
+  minutesHtml.innerHTML = minutes
+  secondsHtml.innerHTML= seconds
+  
+}
+
+ function timerFunction(){
+  const currentDate = new Date().getTime();
+  const countdownDate = new Date(2022, 5, 4, 9, 0, 0, 0).getTime();
+  const distance = new Date(countdownDate - currentDate).getTime();
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  displayTimer(days, hours, minutes, seconds);
+ } 
+
+ 
